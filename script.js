01bc96506935b7fe7c47ec96123527fda40cd8a9
maryamasif2008 <marayam.asif123@gmail.com>
2025-01-18 12:48:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+                                                //CHAPTER # 03
+                    
+                                              /* Question 01 */
+var age=("I am 18 year old");
+alert(age);  
+
+                                             /* Question 02 */
+var num= 1;                             
+num = num + 13;
+alert("You have visit my site" + num + " time");
+
+                                            /* Question 03 */
+var birthYear = 2008; 
+alert("My birth year is " + birthYear); 
+
+                                            /* Question 04 */
+var visitorName = "Bata";
+var productTitle = "Shoes";
+var quantity = 5;
+                       
+var message = visitorName + " ordered " + quantity + " " + productTitle + " Bata shoes shope";
+alert(message);                                                
+                 
+                                  
+ 
+
+             
